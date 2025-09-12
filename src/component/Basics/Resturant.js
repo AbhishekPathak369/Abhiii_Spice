7926161e3cc732045a739c4317cdadad8976a5f6
Abhishek Pathak <pathakabhi290@gmail.com>
2025-09-12 20:49:34 +0530

    Update Resturant.js

--- src/component/Basics/Resturant.js
+++ src/component/Basics/Resturant.js
@@ -36,11 +36,10 @@ const Resturant = () => {
 
         <div className="slogan-container">
           <span className="slogan-text">Where Zaika Meets Zidd</span>
-          <span className="slogan-text">Deliciousness on Every Plate</span>
-          <span className="slogan-text">Taste That Stays With You</span>
+         
         </div>
 
-        
+        <div className="brand-underline"></div>
       </div>
 
       {/* Page content */}
